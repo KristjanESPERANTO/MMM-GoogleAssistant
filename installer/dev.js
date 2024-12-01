85aad3404ad9950ef4d2d0e2bbd926638f112412
bugsounet <github@bugsounet.fr>
2024-12-01 17:35:35 +0100

    rename npm run src to npm run dev

--- installer/dev.js
+++ installer/dev.js
@@ -49,4 +49,6 @@ function install (file) {
   });
 }
 
+console.log("⚠ This Tools is reserved for develop only ⚠\n")
 installFiles();
+console.log("\n✅ All sources files are installed and ready for developing\n")
