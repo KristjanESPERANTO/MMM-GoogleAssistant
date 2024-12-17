eadc1c759b56824a6b7c69b5dc56d820a86637aa
bugsounet <github@bugsounet.fr>
2024-12-18 00:56:59 +0100

    update: EXT-SteamDeck installer

--- EXTs/EXT-StreamDeck/installer/setup.sh
+++ EXTs/EXT-StreamDeck/installer/setup.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 # +-----------------+
-# | npm postinstall |
+# | npm setup |
 # +-----------------+
 
-# get the installer directory
-Installer_get_current_dir () {
-  SOURCE="${BASH_SOURCE[0]}"
-  while [ -h "$SOURCE" ]; do
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-  done
-  echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-}
-
-Installer_dir="$(Installer_get_current_dir)"
-
-# move to installler directory
-cd "$Installer_dir"
-source utils.sh
+source ../../installer/utils.sh
 
 # copy rules
 Installer_info "Installing StreamDeck rules..."
